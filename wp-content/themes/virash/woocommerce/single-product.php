f9d8bde9fccc287a08e81ddeb25e6f7a8effbf31
serg <aspirin_1988@mail.ru>
2016-05-18 12:45:44 +0600

    /start

--- wp-content/themes/virash/woocommerce/single-product.php
+++ wp-content/themes/virash/woocommerce/single-product.php
@@ -2,7 +2,7 @@
 <?php while ( have_posts() ) : the_post();
 	global $post, $product, $woocommerce;
 	$images=$product->get_gallery_attachment_ids();
-	print_r($product->get_gallery_attachment_ids());
+//	print_r($product->get_gallery_attachment_ids());
 	?>
 
 <!-- НАЧАЛО заголовок с декор-рамкой-->
@@ -88,7 +88,7 @@
 
 
 <?php
-/*/**
+/**
  * The Template for displaying all single products
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
@@ -103,41 +103,41 @@
  * @package 	WooCommerce/Templates
  * @version     1.6.4
  */
-
+/*
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-//get_header( 'shop' ); */?><!--
+//get_header( 'shop' ); ?>
 	123
 	<?php
-/*		/**
+		/**
 		 * woocommerce_before_main_content hook.
 		 *
 		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
 		 * @hooked woocommerce_breadcrumb - 20
-		 */
+		 *//*
 		do_action( 'woocommerce_before_main_content' );
-	*/?>
+	?>
 
 
 
 	<?php
-/*		/**
+		/**
 		 * woocommerce_after_main_content hook.
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
+		 *//*
 		do_action( 'woocommerce_after_main_content' );
-	*/?>
+	?>
 
 	<?php
-/*		/**
+		/**
 		 * woocommerce_sidebar hook.
 		 *
 		 * @hooked woocommerce_get_sidebar - 10
-		 */
+		 *//*
 		do_action( 'woocommerce_sidebar' );
-	*/?>
-
---><?php /*get_footer(); */?>
+	?>
+*/
+get_footer(); ?>
