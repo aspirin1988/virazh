a21204a6b29fb43343451cd5b9e97d8e32bd9a3b
serg <aspirin_1988@mail.ru>
2016-05-20 23:27:30 +0600

    /start

--- wp-content/themes/virash/template-parts/content-index.php
+++ wp-content/themes/virash/template-parts/content-index.php
@@ -38,20 +38,108 @@
 <!--НАЧАЛО articles on main -->
 <div class="articles-on-main container">
 	<h2>Сегодня в автосалонах вираж</h2>
-	<div class="row">
-		<div class="square"></div>  <!--синяя полурамка -->
-		<?php
-		$args = array( 'category_name'=> 'news' ,'numberposts'=>5 , 'order'=>'ASC' );
-		$categories=get_posts($args );
-		foreach ($categories as $key=> $value) :
-		?>
-		<div class="col-sm-<?php if ($key==3): ?>8<?php else :?>4<?php endif; ?> article">
-			<a href="<?=get_permalink($value->ID)?>">
-				<img src="<?=get_the_post_thumbnail_url($value->ID)?>">
-				<h3><?=$value->post_title?></h3>
-			</a>
+	<div id="carousel-articles" class="carousel slide" data-ride="carousel">
+		<?php $args = array( 'category_name'=> 'slider' ,'numberposts'=>100 , 'order'=>'ASC' );
+		$categories=get_posts($args );?>
+
+		<ol class="carousel-indicators">
+			<?php $col=1; $class='active'; foreach ($categories as $key=> $value) : if ($col==3): ?>
+			<li data-target="#carousel-articles" data-slide-to="<?=$key?>" class="active"></li>
+			<?php $col=1; $class=''; endif; $col++; endforeach; ?>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox" data-interval="20000">
+			<div class="item active">
+
+				<img src="<?=bloginfo('template_directory')?>/public/img/index/articles-sample2.jpg" alt="Статья">
+				<div class="carousel-caption">
+					<div class="article-preview">
+						<h4>Статья 1</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+							aliquam aliquid ea facere facilis inventore.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg visible-md">
+						<h4>Статья 2</h4>
+						<p>Lorem ipsum dolor sit amet magnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg">
+						<h4>Очень очень длинное название статьи, прямо-таки вредное.</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vagnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?=bloginfo('template_directory')?>/public/img/index/articles-sample.jpg" alt="Статья">
+				<div class="carousel-caption">
+					<div class="article-preview">
+						<h4>Статья 1</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+							aliquam aliquid ea facere facilis inventore.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg visible-md">
+						<h4>Статья 2</h4>
+						<p>Lorem ipsum dolor sit amet magnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg">
+						<h4>Очень очень длинное название статьи, прямо-таки вредное.</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vagnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?=bloginfo('template_directory')?>/public/img/index/articles-sample3.jpg" alt="Статья">
+				<div class="carousel-caption">
+					<div class="article-preview">
+						<h4>Статья 1</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+							aliquam aliquid ea facere facilis inventore.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg visible-md">
+						<h4>Статья 2</h4>
+						<p>Lorem ipsum dolor sit amet magnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+					<div class="article-preview visible-lg">
+						<h4>Очень очень длинное название статьи, прямо-таки вредное.</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vagnam minus mollitia omnis repellat sed veniam
+							voluptatem.
+						</p>
+						<p><a href="#">Читать далее..</a></p>
+					</div>
+				</div>
+			</div>
 		</div>
-		<?php endforeach; ?>
+
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-articles" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#carousel-articles" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div>
 </div>
 <!--КОНЕЦ articles on main -->
