d4806668edc2641170fc6060d0cadbec97c4d27b
serg <aspirin_1988@mail.ru>
2016-05-23 11:12:35 +0600

    /start

--- wp-content/themes/virash/woocommerce/archive-product.php
+++ wp-content/themes/virash/woocommerce/archive-product.php
@@ -175,15 +175,7 @@ function collapse ($level,$filter){
 <!-- НАЧАЛО список статей-->
 <div class="container products-catalog">
 	<div class="row">
-		<h3>Фильтр</h3>
-		<div class="filter-button navbar-toggle collapsed hidden-md" onclick="$('.filter').toggle(200)" >
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</div>
-		<br>
-		<br>
-		<br>
+		<h3 class="filter-title" onclick="$('.filter').toggle(200)">Фильтр </h3>
 		<form id="form_filter" action="" method="get" class="col-sm-3 filter hidden-sm ">
 			<input type="button" onclick="clearfilter()" value="Очистит фильтр" class="btn" >
 			<br>
